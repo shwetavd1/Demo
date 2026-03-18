9e85e46106b7e29b772bbd7b22d80679b1eda1c2
dalvi <shweta.dalvi1@zensar.com>
2026-03-18 15:01:48 +0530

    checking cherry pick command

--- Program.cs
+++ Program.cs
@@ -10,3 +10,9 @@ Console.WriteLine("hello from feature-day1 branch");
 
 Console.WriteLine("change by A");
 Console.WriteLine("change by B");
+Console.WriteLine("new changes");
+
+int d = 8;
+Console.WriteLine(d);
+
+Console.WriteLine("cherry pick");
